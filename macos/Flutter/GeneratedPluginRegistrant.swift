7356823a16ea031ff139a0c5ae7f18c8596b9c15
Alan Paul <alanpaul5433@gmail.com>
2026-08-04 04:12:05 +0500

    chore: regenerate macOS/Windows plugin registrants for Firebase packages
    Flutter-generated registrant updates from adding firebase_core,
    firebase_auth, and cloud_firestore in the prior commit. Purely additive:
    new import/include lines and RegisterPlugins entries for the three
    packages, no other changes.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,11 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import device_info_plus
 import file_picker
+import firebase_auth
+import firebase_core
 import flutter_local_notifications
 import flutter_secure_storage_darwin
 import flutter_timezone
@@ -17,8 +20,11 @@ import share_plus
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
